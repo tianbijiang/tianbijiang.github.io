f731a92a5cc699db268b10cac793fbccaad29f16
Tianbi <tianbi.jiang@gmail.com>
2016-03-09 14:05:16 -0600

    video smoothify

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
         this.play();
     });
 
+
     window.requestAnimFrame = (function () {
         return window.requestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
